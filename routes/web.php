9d27ae80f00ae6873d09be719328369876777b26
samandar <xkas2001@gmail.com>
2022-12-30 21:35:01 +0500

    User login done

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,7 @@ Route::get('/register', [UserController::class, 'create']);
 Route::post('/users', [UserController::class, 'store']);
 
 Route::post('/logout', [UserController::class, 'logout']);
+
+Route::get('/login', [UserController::class, 'login']);
+
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
