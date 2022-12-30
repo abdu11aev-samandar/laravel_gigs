daa05faf220a65779e5ddaff28c975307ff83947
samandar <xkas2001@gmail.com>
2022-12-30 14:44:46 +0500

    working with View

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,2 +1,8 @@
-<h2>{{ $listing['title'] }}</h2>
-<p>{{ $listing['description'] }}</p>
+@extends('layout')
+
+@section('content')
+
+    <h2>{{ $listing['title'] }}</h2>
+    <p>{{ $listing['description'] }}</p>
+
+@endsection
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,18 +1,21 @@
-<h1>{{ $heading }}</h1>
+@extends('layout')
 
-{{--@if(count($listings)==0)
-    <p>No listings found</p>
-@endif--}}
+@section('content')
 
-@unless(count($listings)==0)
+    <h1>{{ $heading }}</h1>
 
-    @foreach($listings as $listing)
-        <h2>
-            <a href="/listings/{{$listing['id']}}">{{ $listing['title'] }}</a>
-        </h2>
-        <p>{{ $listing['description'] }}</p>
-    @endforeach
+    @unless(count($listings)==0)
 
-@else
-    <p>No listings found</p>
-@endunless
+        @foreach($listings as $listing)
+            <h2>
+                <a href="/listings/{{$listing['id']}}">{{ $listing['title'] }}</a>
+            </h2>
+            <p>{{ $listing['description'] }}</p>
+        @endforeach
+
+    @else
+        <p>No listings found</p>
+    @endunless
+
+@endsection
+
